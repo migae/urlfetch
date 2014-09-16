9a93c6f9efdbf276d7d6aff8744e4095e85581ee
Gregg Reynolds <dev@mobileink.com>
2014-09-16 08:53:11 -0500

    migrate from migae repo

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject migae/urlfetch "0.1.0-SNAPSHOT"
+  :description "migae - Mobile Ink Google App Engine sdk for Clojure."
+  :url "https://github.com/migae/urlfetch"
+  :min-lein-version "2.0.0"
+  :aot [#".*"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.9.10"]]
+  :repositories {"mvncentral" "mvnrepository.com"}
+  :profiles {:dev {:dependencies
+                   [[org.clojure/tools.logging "0.2.3"]]}})
